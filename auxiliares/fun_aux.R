149965fa95631664f1e6c1044ef38fa8246fa72f
imoralesl <imoralesl@cdmx.gob.mx>
2025-12-27 13:25:53 -0600

    subir pruebas

---
+++ auxiliares/fun_aux.R
@@ -0,0 +1,60 @@
+
+## Esta función toma como argumento x que es el texto abreviado de los meses y devuelve el valor del mes en entero
+mes_abr <- function(x) { 
+  r <- case_when( 
+    x == "Ene" ~ 1, 
+    x == "Feb" ~ 2,
+    x == "Mar" ~ 3,
+    x == "Abr" ~ 4,
+    x == "May" ~ 5,
+    x == "Jun" ~ 6,
+    x == "Jul" ~ 7,
+    x == "Ago" ~ 8,
+    x == "Sep" ~ 9,
+    x == "Oct" ~ 10, 
+    x == "Nov" ~ 11,
+    x == "Dec" ~ 12 )  
+  return(r)
+  
+}
+
+### Diccionario de meses para transformar a Date
+
+meses <- c("enero", "febrero", "marzo", "abril", "mayo", "junio", 
+           "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre")
+
+
+## Esta función toma como argumento st que es el nombre del archivo del SENSP y te regresa los días transcuridos hasta la fecha de corte
+
+dias_corte_sesnp <- function(st) {
+  anio_corte <- substr(st, 31, 34)
+  mes_corte <- substr(st, 28, 30)
+  mes_corte <- str_to_title(mes_corte)
+  mes_corte <- mes_abr(mes_corte)
+  fecha_corte_foo <- paste0(anio_corte,'-', mes_corte, '-01')
+  fecha_corte <- ceiling_date(as.Date(fecha_corte_foo), unit = "month") - days(1)
+  dias_anio <- yday(fecha_corte)
+  return(dias_anio) }
+
+
+#### 
+
+tema_entropia <- 
+  theme(
+    plot.title = element_text(size = 30, face = "bold"), # Font size set to 16 and bold.
+    panel.border = element_blank(), 
+    panel.grid.major.y = element_line(color = "#EDECEC",
+                                      linewidth = 1,
+                                      linetype =1),    
+    
+    #panel.grid.major = element_blank(),
+    #panel.grid.minor = element_blank(), 
+    axis.text=element_text(size=15 ,face="bold"),
+    axis.title=element_text(size=18,face="bold"),
+    plot.caption = element_text(hjust = 0.05) ,
+    plot.caption.position = "plot",
+    plot.margin = margin(4, 4, 4, 4),
+    axis.line = element_blank(),
+    panel.background = element_rect(fill =  'white' )
+    #fill, colour, size, linetype, color)
+  )
--- procesamiento/serie-tiempo.R
+++ procesamiento/serie-tiempo.R
@@ -1,7 +1,67 @@
 # El archivo fun_aux.R contiene algunas funciones útiles para el procesamiento de datos
 
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(dplyr, readr, tidyr, readxl,janitor,ggplot2,
-               here, stringr, lubridate, purrr, forecast)
+library(here)
+pacman::p_load(dplyr, readr, tidyr, janitor,ggplot2,
+               stringr, lubridate, purrr, forecast)
 
-source("fun_aux.R")
+source("../auxiliares/fun_aux.R")
+
+# Leer el archivo
+h <- read_csv(('../datos/conteo_mes.csv'))
+
+# Ver que se cargó bien
+glimpse(h)
+
+## Desarrollar una serie de tiempo
+ts_h <- h |> 
+  group_by(mes_ts) %>%
+  summarise(t = sum(conteo))
+
+# Con base en esto 
+dim(ts_h)
+tail(ts_h)
+
+###
+
+ts_h <- ts_h[ -132, ]
+tail(ts_h)
+
+### Serie de tiempo mensual
+
+ts_mes <- ts(ts_h$t , 
+             start = c(as.numeric(substr(ts_h$mes_ts[1], 1, 4)), 
+                       as.numeric(substr(ts_h$mes_ts[1], 6, 7))), 
+             frequency = 12)
+
+# Lo primero que queremos hacer es graficar la serie.
+plot.ts(ts_mes)
+
+
+### Descomponemos la serie 
+
+dec_ts <- decompose(ts_mes)
+plot(dec_ts)
+
+stl_decomp <- stl(ts_mes , s.window = 12)#, t.window = 15)
+#s.window = "periodic")
+plot(stl_decomp)
+
+
+## Holt winters
+
+ht <- HoltWinters(ts_mes) #, beta=FALSE, gamma=FALSE)
+
+ht
+
+plot(ht)
+
+ht_forec <- hw(ts_mes, seasonal = "additive", h = 2)
+
+plot(ht_forec)
+
+ht_forec$model
+
+ht_forec
+
+
